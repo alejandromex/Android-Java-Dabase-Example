ed782780aa3de4cb1bd4596044d3fbceaa446877
Alejandro Velazquez <alexandro_sin@hotmail.com>
2021-03-21 14:21:50 -0700

    DataBase ready, CRUD USER AND AGES

--- app/src/main/java/com/example/databaseexample/DataManager.java
+++ app/src/main/java/com/example/databaseexample/DataManager.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 import androidx.annotation.Nullable;
 
@@ -30,24 +31,32 @@ public class DataManager {
 
     public void InsertAUser(String name, String age)
     {
-
+        String query = "INSERT INTO "+TABLE_NAMES_AND_ADDRESS+" ("+
+                TABLE_ROW_NAME+", "+TABLE_ROW_AGE+") "+
+                "VALUES ('"+name+"', "+"'"+age+"');";
+        Log.i("Metodo: INSERT()", query);
+        db.execSQL(query);
     }
 
     public void DeleteAUser(String name)
     {
-
+        String query = "DELETE FROM "+TABLE_NAMES_AND_ADDRESS+" WHERE "+TABLE_ROW_NAME+" = "+"'"+name+"'";
+        Log.i("Metodo: DELETE()", query);
+        db.execSQL(query);
     }
 
     public Cursor GetAUser(String name)
     {
-
-        return null;
+        String query = "SELECT * FROM "+TABLE_NAMES_AND_ADDRESS+" WHERE "+TABLE_ROW_NAME+" = "+"'"+name+"'";
+        Cursor c = db.rawQuery(query, null);
+        return c;
     }
 
     public Cursor SelectAll()
     {
-
-        return null;
+        String query = "SELECT * FROM "+TABLE_NAMES_AND_ADDRESS;
+        Cursor c = db.rawQuery(query, null);
+        return c;
     }
 
 
@@ -59,12 +68,41 @@ public class DataManager {
 
         @Override
         public void onCreate(SQLiteDatabase db) {
+            String newTablesQuery = "CREATE TABLE "+
+                                    TABLE_NAMES_AND_ADDRESS + " ("+
+                                    TABLE_ROW_ID + " INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "+
+                                    TABLE_ROW_NAME+ " text not null, "+
+                                    TABLE_ROW_AGE+ " text not null);";
+
+            db.execSQL(newTablesQuery);
 
         }
 
         @Override
         public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            if(oldVersion < newVersion)
+            {
+                for(int i = oldVersion+1; i<= newVersion; i++)
+                {
+                    Log.i("UPGRADING database", "Estamos haciendo la actualizacion de nuestro base de dates");
+                    updateToVersion(i);
+                }
+            }
+        }
 
+        private void updateToVersion(int versionToUpdate)
+        {
+            switch(versionToUpdate)
+            {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                default:
+                    break;
+            }
         }
     }
 
--- app/src/main/java/com/example/databaseexample/MainActivity.java
+++ app/src/main/java/com/example/databaseexample/MainActivity.java
@@ -2,13 +2,87 @@ package com.example.databaseexample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnInsert;
+    Button btnDelete;
+    Button btnSearch;
+    Button btnGetAll;
+    TextView txtInsertName;
+    TextView txtInsertAge;
+    TextView txtDelete;
+    TextView txtSearch;
+
+    DataManager dm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dm = new DataManager(this);
+
+        btnInsert = findViewById(R.id.btnInsert);
+        btnDelete = findViewById(R.id.btnDelete);
+        btnSearch = findViewById(R.id.btnSearch);
+        btnGetAll = findViewById(R.id.btnSelectAll);
+
+        txtInsertName = findViewById(R.id.txtName);
+        txtInsertAge = findViewById(R.id.txtAge);
+        txtDelete = findViewById(R.id.txtDelete);
+        txtSearch = findViewById(R.id.txtSearch);
+
+        btnInsert.setOnClickListener(this);
+        btnDelete.setOnClickListener(this);
+        btnSearch.setOnClickListener(this);
+        btnGetAll.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.btnDelete)
+        {
+            if(txtDelete.getText() != "")
+            {
+                dm.DeleteAUser(txtDelete.getText().toString());
+            }
+        }
+        else if(v.getId() == R.id.btnInsert)
+        {
+            if(txtInsertName.getText() != "" && txtInsertAge.getText() != "")
+            {
+                dm.InsertAUser(txtInsertName.getText().toString(), txtInsertAge.getText().toString());
+            }
+        }
+
+        else if(v.getId() == R.id.btnSearch)
+        {
+            if(txtSearch.getText() != "")
+            {
+                showData(dm.GetAUser(txtSearch.getText().toString()));
+            }
+        }
+        else if(v.getId() == R.id.btnSelectAll)
+        {
+            showData(dm.SelectAll());
+        }
+
+    }
+
+    public void showData(Cursor c)
+    {
+        while(c.moveToNext())
+        {
+            Toast.makeText(this,c.getString(1)+" "+c.getString(2),Toast.LENGTH_LONG).show();
+            Log.i(c.getString(1), c.getString(2));
+        }
     }
 }
